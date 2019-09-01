d373638245c2d91b2b1ab2632a361c478bb052be
kunwei-kooboo <kunwei@kooboo.com>
2019-09-01 19:34:04 +0800

    build: Add stylelint

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,9 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module'
   ],
   /*
    ** Nuxt.js modules
