05e4f85986527ca36ec28f28130d7260ab1b6e73
kunwei-kooboo <kunwei@kooboo.com>
2019-09-30 14:22:43 +0800

    chore: Make sense for comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   extends: [
-    '@nuxtjs/eslint-config-typescript', // include `eslint-plugin-jest`, `eslint-plugin-vue`
-    'prettier',
+    '@nuxtjs/eslint-config-typescript', // include `eslint-config-standard`, `eslint-plugin-jest`, `eslint-plugin-vue`, `eslint-plugin-unicorn`, `@typescript-eslint/eslint-plugin`
     'plugin:@typescript-eslint/recommended',
-    'plugin:nuxt/recommended',
-    'prettier/@typescript-eslint',
-    // 'plugin:prettier/recommended' // this plugin disable all other eslint rules relating to prettier
+    'prettier', // turn `off` some `eslint` rules by `prettier`
+    'prettier/@typescript-eslint', // turn `off` some `@typescript-eslint/recommended` rules by `prettier`
+    'plugin:nuxt/recommended', // this use `parser: 'vue-eslint-parser'`, should be list after 'plugin:@typescript-eslint/recommended' (use parser: '@typescript-eslint/parser')
+    // 'plugin:prettier/recommended' // this plugin only enable prettier recommended rules and enforce disable all other `eslint` rules
   ],
   // add your custom rules here
   rules: {
+    'vue/no-v-html': 'off', // recommended by vue, but `off` for practical usage, if you are certain the content passed to v-html is sanitized HTML.
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -76,19 +76,6 @@ export default {
    */
   build: {
     extractCSS: true,
-    postcss: {
-      // Add plugin names as key and arguments as value
-      // Install them before as dependencies with npm or yarn
-      plugins: {
-        // Disable a plugin by passing false as value
-      },
-      preset: {
-        // Change the postcss-preset-env settings
-        autoprefixer: {
-          grid: true
-        }
-      }
-    },
     /*
      ** You can extend webpack config here
      */
