248b44a57fedb78897d3c29e8110b43ff52a3441
kunwei-kooboo <kunwei@kooboo.com>
2019-10-10 13:43:33 +0800

    chore: Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
   extends: [
     '@nuxtjs/eslint-config-typescript', // include `eslint-config-standard`, `eslint-plugin-jest`, `eslint-plugin-vue`, `eslint-plugin-unicorn`, `@typescript-eslint/eslint-plugin`
     'plugin:@typescript-eslint/recommended',
-    // 'prettier', // turn `off` some `eslint` rules by `prettier`
-    // 'prettier/@typescript-eslint', // turn `off` some `@typescript-eslint/recommended` rules by `prettier`
     'plugin:nuxt/recommended', // this use `parser: 'vue-eslint-parser'`, should be list after 'plugin:@typescript-eslint/recommended' (use parser: '@typescript-eslint/parser')
-    // 'plugin:prettier/recommended' // this plugin only enable prettier recommended rules and enforce disable all other `eslint` rules
   ],
   // add your custom rules here
   rules: {
